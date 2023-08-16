de2fcdb904aa088f37fdb734a008d3b962e6e68b
gusttt <gusttt@users.noreply.github.com>
2023-08-16 19:09:56 +0200

    mmbarbot.py: Refactor + allow to specify commit for testing

--- mmbot/mmbarbot.py
+++ mmbot/mmbarbot.py
@@ -68,57 +68,82 @@ class MattermostAPI:
             logging.error("Error: Failed to post direct message. Status code: %s %s", response.status_code, response.text)
 
 
-def get_mm_user(user):
-    member_name = user
-    # Load the members data from the 'members.beancount' file
-    members_file = "./static/members.beancount"
-    with open(members_file, "r") as f:
-        members_data = f.read()
-    # Parse the members data using beancount
-    members_entries, _, _ = loader.load_string(members_data)
-    # Find and extract the mm_name for the target member
-    mm_name = None
-    for member_entry in members_entries:
-        logging.debug('Entry: %s', member_entry)
-        logging.debug('Account: %s', member_entry.account)
-        logging.debug('Display name: %s', member_entry.meta.get("display_name"))
-        if (
-            member_entry.__class__.__name__ == "Open" and
-            member_entry.account == f"Liabilities:Bar:Members:{member_name}" or
-            member_entry.meta.get("display_name") == f"{member_name}"
-        ):
-            mm_name = member_entry.meta.get("mm_name")
-            break
+class BeancountParser():
+    def get_mm_user(self, user):
+        member_name = user
+        # Load the members data from the 'members.beancount' file
+        members_file = "./static/members.beancount"
+        with open(members_file, "r") as f:
+            members_data = f.read()
+        # Parse the members data using beancount
+        members_entries, _, _ = loader.load_string(members_data)
+        # Find and extract the mm_name for the target member
+        mm_name = None
+        for member_entry in members_entries:
+            logging.debug('Entry: %s', member_entry)
+            logging.debug('Account: %s', member_entry.account)
+            logging.debug('Display name: %s', member_entry.meta.get("display_name"))
+            if (
+                member_entry.__class__.__name__ == "Open" and
+                member_entry.account == f"Liabilities:Bar:Members:{member_name}" or
+                member_entry.meta.get("display_name") == f"{member_name}"
+            ):
+                mm_name = member_entry.meta.get("mm_name")
+                break
 
-    if mm_name:
-        logging.info('Mattermost user for %s is: %s', member_name, mm_name)
-        return mm_name
-    else:
-        logging.info('Mattermost user (mm_name) not found for %s in members.beancount. Stopping here.', member_name)
-        sys.exit()
-
-
-def get_user_from_tx(added_transactions):
-    # Parse the transaction using beancount
-    tx_entries, _, _ = loader.load_string(added_transactions)
-
-    # Extract the member name from the transaction description
-    transaction_description = tx_entries[0].narration
-    member_name = transaction_description.split(" ")[0]
-    return member_name
+        if mm_name:
+            logging.info('Mattermost user for %s is: %s', member_name, mm_name)
+            return mm_name
+        else:
+            logging.info('Mattermost user (mm_name) not found for %s in members.beancount. Stopping here.', member_name)
+            sys.exit()
 
 
-def extract_added_transactions_from_git_show():
-    result = subprocess.run(['git', 'show'], stdout=subprocess.PIPE)
-    output = result.stdout.decode('utf-8')
-    lines = output.splitlines()
-    tx = ""
-    for line in lines:
-        if line.startswith('+'):
-            if line.startswith('++'):
-                continue
-            tx = tx + line.lstrip('+') + '\n'
-    return tx
+    def get_user_from_tx(self, added_transactions):
+        # Parse the transaction using beancount
+        tx_entries, _, _ = loader.load_string(added_transactions)
+
+        # Extract the member name from the transaction description
+        transaction_description = tx_entries[0].narration
+        member_name = transaction_description.split(" ")[0]
+        return member_name
+
+
+    def extract_added_transactions_from_git_patch(self, commit_hash=None):
+        command = ''
+        if commit_hash is None:
+                command = ['git', 'log', '-1', '-p']
+        else:
+                command = ['git', 'log', '-1', '-p', commit_hash]
+                
+        result = subprocess.run(command, stdout=subprocess.PIPE)
+        output = result.stdout.decode('utf-8')
+
+        lines = output.splitlines()
+        tx = ""
+        for line in lines:
+            if line.startswith('+'):
+                if line.startswith('++'):
+                    continue
+                tx = tx + line.lstrip('+') + '\n'
+        return tx
+    
+
+    def check_if_transaction(self, commit_hash=None):
+        command = ''
+        if commit_hash is None:
+                command = ['git', 'log', '-1', '--format=%s']
+        else:
+                command = ['git', 'log', '-1', '--format=%s', commit_hash]
+                
+        result = subprocess.run(command, stdout=subprocess.PIPE)
+        output = result.stdout.decode('utf-8').rstrip()
+        if output != "Automatic commit by backtab":
+            logging.info("Commit msg '%s' does not match 'Automatic commit by backtab', assuming not a transaction.", output)
+            return False
+        else:
+            return True
+            sys.exit(0)
 
 
 if __name__ == '__main__':
@@ -128,19 +153,21 @@ if __name__ == '__main__':
     # Hardcoded from github actions yml
     mm_url = sys.argv[1]
     token = sys.argv[2]
+    if len(sys.argv) == 4:
+        git_commit_hash = sys.argv[3]
+    else:
+        git_commit_hash = None
 
-    # Check if the commit is transaction 
-    result = subprocess.run(['git', 'log','-1','--format=%s'], stdout=subprocess.PIPE)
-    output = result.stdout.decode('utf-8').rstrip()
-    if output != "Automatic commit by backtab":
-        logging.info("Commit msg does not match 'Automatic commit by backtab', assuming not a transaction. Stopping here.")
+    bcparser = BeancountParser()
+    if not bcparser.check_if_transaction(git_commit_hash):
+        logging.info('Not a transaction, stopping here.')
         sys.exit(0)
 
-    added_transactions = extract_added_transactions_from_git_show()
+    added_transactions = bcparser.extract_added_transactions_from_git_patch(git_commit_hash)
     logging.info('Git commit msg:\n %s', added_transactions)
-    member_name = get_user_from_tx(added_transactions)
+    member_name = bcparser.get_user_from_tx(added_transactions)
     logging.info('Membername from transaction: %s', member_name)
-    mm_user = get_mm_user(member_name)
+    mm_user = bcparser.get_mm_user(member_name)
 
     api = MattermostAPI(mm_url, token)
     # Setup mattermost direct message channel between barbot and transaction user
